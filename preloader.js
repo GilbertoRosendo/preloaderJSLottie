cf1b7ed1962caa8e9fe0049e15cf007d0ab1679f
GilbertoRosendo <gilberto.gc@hotmail.com>
2024-09-05 14:58:36 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -38,7 +38,7 @@ function hideOverlay() {
         renderer: 'svg', // ou 'canvas' se preferir
         loop: true,
         autoplay: true,
-        path: 'https://GilbertoRosendo.github.io/preloaderJSLottie/Animation - 1725556783538.json',
+        path: 'https://gilbertorosendo.github.io/preloaderJSLottie/Animation-1725556783538.json',
     });
 }
 
